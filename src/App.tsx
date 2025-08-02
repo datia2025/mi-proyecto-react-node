714bff9af1b750024351e1aba26f6f15e1ce65b4
Datia <info@talentracking.com>
2025-08-02 11:20:19 -0500

    Revert "pagina promo 20% descuento"
    This reverts commit 518e4f7a546d3588ddefa7db4cc4234591d1af77.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ import OperationsPage from './pages/solutions/OperationsPage';
 import SmartMarketingPage from './pages/solutions/SmartMarketingPage';
 import ChatbotsPage from './pages/solutions/ChatbotsPage';
 import WebAppsPage from './pages/solutions/WebAppsPage';
-import LearnPageDiscount from './pages/learn-discount'; 
 
 function App() {
   return (
@@ -37,8 +36,6 @@ function App() {
             <Route path="/soluciones/marketing" element={<SmartMarketingPage />} />
             <Route path="/soluciones/chatbots" element={<ChatbotsPage />} />
             <Route path="/soluciones/apps-web" element={<WebAppsPage />} />
-            <Route path="/learn-discount" element={<LearnPageDiscount />} /> {/* <-- AÑADE ESTA LÍNEA */}
-
           </Routes>
         </main>
         <Footer />
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 👈 cambia esto a `/` si estás en la raíz de datia.digital
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
